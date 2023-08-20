ec1d5cf08cbca2f9865468af18abb938c484f742
Ramiz <106487380+RamizMamedovV@users.noreply.github.com>
2023-08-20 14:27:52 +0300

    добавил ответ на текстовый запрос без '/'

--- new_bot.py
+++ new_bot.py
@@ -4,6 +4,8 @@ import json
 import requests
 films=[]
 
+API_URL='https://7012.deeppavlov.ai/model'
+
 API_TOKEN = '6574903644:AAGy5ukTAeNdI1KrQb28fRRfeXBp0wQL9ks'
 bot = telebot.TeleBot(API_TOKEN) # инициализируем конструктор Телебот
 
@@ -30,5 +32,28 @@ def save_all(message):
         fh.write(json.dumps(films,ensure_ascii=False))
     bot.send_message(message.chat.id,"Фильмотека сохранена в films.json")
    
+@bot.message_handler(commands=['wiki'])
+def wiki(message):
+    quest = message.text.split()[1:] # считываем строку, но без начального 'wiki'
+    qq=" ".join(quest)               # получаем строковое значение введенного запроса
+    data = { 'question_raw': [qq]}
+    try:
+        # Get - для получения а post - для отправки в HHTP
+        # с помощью запроса requests.post по адресу "API_URL", с "json=data"
+        # м отключает проверку сертификата SSL - хотя так делать не нужно
+    # json() разбирает JSON-ответ,от сервера, и возвращает его в виде объекта Python.
+        res = requests.post(API_URL,json=data,verify=False).json()
+        bot.send_message(message.chat.id, res) # отправим пользователю то, что получили
+    except:
+        bot.send_message(message.chat.id, "Ничего не нашёл » :-(")  
+
+
+# чтобы некоторые команды не воспринимались как тестовые, 
+# этот декоратор лучше ставить в конце
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if "дела" in message.text.lower() :
+       bot.send_message(message.chat.id, "Нормально, Сам как?")
+
 
 bot.polling() # запуск бот-а, здесь спрятан 'while(True)'
